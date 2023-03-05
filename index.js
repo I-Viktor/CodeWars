0ddebb03e3a7c18666017b36ffe5059761be9456
I-Viktor <Lafasolka@gmail.com>
2023-03-05 15:14:26 +0200

    first__commit

---
+++ index.js
@@ -0,0 +1,26 @@
+//Training JS #11: loop statement --break,continue
+//https://www.codewars.com/kata/5721c189cdd71194c1000b9b/solutions/javascript
+
+function grabDoll(dolls) {
+    var bag = [];
+    //coding here
+    for (const name of dolls) {
+        if (name === "Hello Kitty" || name === "Barbie doll") {
+            bag.push(name);
+        }
+        if (bag.length === 3) {
+            break;
+        }
+        else {
+            continue;
+        }
+    }
+
+    return bag;
+}
+
+
+grabDoll(["Mickey Mouse", "Hello Kitty", "Snow white"]   //),["Hello Kitty"]);
+grabDoll(["Mickey Mouse", "Hello Kitty", "Hello Kitty", "Snow white"]   //),["Hello Kitty","Hello Kitty"]);
+grabDoll(["Mickey Mouse", "Hello Kitty", "Hello Kitty", "Barbie doll", "Snow white"]   //),["Hello Kitty","Hello Kitty","Barbie doll"]);
+grabDoll(["Mickey Mouse", "Barbie doll", "Hello Kitty", "Hello Kitty", "Hello Kitty", "Snow white"]   //),["Barbie doll","Hello Kitt   //
