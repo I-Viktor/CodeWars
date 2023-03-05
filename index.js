4610db3fcd865a67817d6090b80ef330babb465c
I-Viktor <Lafasolka@gmail.com>
2023-03-05 15:19:29 +0200

    terminal__commit

--- index.js
+++ index.js
@@ -21,6 +21,9 @@ function grabDoll(dolls) {
 
 
 grabDoll(["Mickey Mouse", "Hello Kitty", "Snow white"]   //),["Hello Kitty"]);
+
 grabDoll(["Mickey Mouse", "Hello Kitty", "Hello Kitty", "Snow white"]   //),["Hello Kitty","Hello Kitty"]);
+
 grabDoll(["Mickey Mouse", "Hello Kitty", "Hello Kitty", "Barbie doll", "Snow white"]   //),["Hello Kitty","Hello Kitty","Barbie doll"]);
+
 grabDoll(["Mickey Mouse", "Barbie doll", "Hello Kitty", "Hello Kitty", "Hello Kitty", "Snow white"]   //),["Barbie doll","Hello Kitt   //
