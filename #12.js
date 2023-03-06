f5d8e426f89b6f8b3c70d656f84c1d0a816f9a6a
I-Viktor <Lafasolka@gmail.com>
2023-03-06 14:29:15 +0200

    push_#12

---
+++ #12.js
@@ -0,0 +1,22 @@
+// Training JS #12: loop statement --for..in and for..of
+// https://www.codewars.com/kata/5722b3f0bd5583cf44001000/train/javascript
+
+
+function giveMeFive(obj) {
+    //coding here
+    const arr = []
+    for (let key in obj) {
+        if (key.length === 5) {
+            arr.push(key)
+        }
+        if (obj[key].length === 5) {
+            arr.push(obj[key])
+        }
+    }
+    return arr
+}
+
+
+giveMeFive({ Our: "earth", is: "a", beautyful: "world" })//,["earth","world"]);
+giveMeFive({ Ihave: "enough", money: "to", buy: "a", car: "model" })//,["Ihave","money","model"]);
+giveMeFive({ Pears: "than", apple: "sweet" })//,["Pears",//
