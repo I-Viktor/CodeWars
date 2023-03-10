125f0617e92aa6fac7a14d282f8db05c6086ecc2
I-Viktor <Lafasolka@gmail.com>
2023-03-10 16:14:32 +0200

    final

--- #14.js
+++ #14.js
@@ -1,17 +1,25 @@
+//Training JS #14: Methods of Number object--toString() and toLocaleString()
+//https://www.codewars.com/kata/57238ceaef9008adc7000603
+
+
 function colorOf(r, g, b) {
-    let red = r.toString(16); (red < 16) ? '0' + red 
-    let green = g.toString(16); (green < 16) ? '0' + green;
-    let blue = b.toString(16); (blue < 16) ? '0' + blue;
+    let red = r.toString(16);
+    let green = g.toString(16);
+    let blue = b.toString(16);
+
+    red = red.length < 2 ? '0' + red : red;
+    green = green.length < 2 ? '0' + green : green;
+    blue = blue.length < 2 ? '0' + blue : blue;
 
 
 
-    return '#' + ${ red } + ${ green } + ${ blue };
+    return `#${red}${green}${blue}`;
 }
 
 
 
-
 // Irena 
+/*
 function colorOf(r, g, b) {
     let colorR = ${
         r <= 15 ? "0" : ""
@@ -29,4 +37,4 @@ let colorB = ${
 
 return '#' + colorR + colorG + colorB 
     }
-
+*/
