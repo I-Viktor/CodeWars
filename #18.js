b18fb57fb52dd7348a44631f3b80e6617d0a044f
I-Viktor <Lafasolka@gmail.com>
2023-03-15 18:31:23 +0200

    final_result

--- #18.js
+++ #18.js
@@ -1,13 +1,17 @@
 // Training JS #18: Methods of String object--concat() split() and its good friend join()
 //https://www.codewars.com/kata/57280481e8118511f7000ffa/train/javascript
 
-function splitAndMerge(string, separator) {
-    const words = string.split('')/*.filter(simbol => simbol !== ' ')*/.join(separator);
 
-    return words;
+function splitAndMerge(str, separator) {
+
+    let words = str.split(" ");
+
+    let charsJoined = words.map(word => word.split("").join(separator));
+
+    let result = charsJoined.join(" ");
+
+    return result;
 }
-
-
 
 splitAndMerge("My name is John", " ");// "M y n a m e i s J o h n");
 splitAndMerge("My name is John", "-");// "M-y n-a-m-e i-s J-o-h-n");
