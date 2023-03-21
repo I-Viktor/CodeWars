80c052a4b7f525f5b0e585cdc75b741fa430e027
I-Viktor <Lafasolka@gmail.com>
2023-03-21 14:29:53 +0200

    finaliti

---
+++ #22.js
@@ -0,0 +1,13 @@
+//  https://www.codewars.com/kata/572ab0cfa3af384df7000ff8
+
+function shuffleIt(arr, ...x) {
+
+
+}
+
+
+
+
+shuffleIt([1, 2, 3, 4, 5], [1, 2]),       // [1, 3, 2, 4, 5]);
+    shuffleIt([1, 2, 3, 4, 5], [1, 2], [3, 4]),   // [1, 3, 2, 5, 4]);
+    shuffleIt([1, 2, 3, 4, 5], [1, 2], [3, 4], [2, 3])   // [1, 3, 5, 2, 4]);
